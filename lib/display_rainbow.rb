8cd65796cfc71eb8c9f74f8db868e9699161f811
Abigail Parisienne <abigail.parisienne@wework.com>
2018-02-20 21:48:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
+=======
+def display_board(colors)
+  puts " R: # {colors[0]}, O: # {colors[1]}, Y: # {colors[2]}, G: # {colors[3]}, B: # {colors[4]}, I: # {colors[5]}, V: # {colors[6]}"
+end
+end
+
+>>>>>>> 54fbcb838ce53e0f03ce733a612a27a520c76337
